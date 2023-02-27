32a2db5b06f951b0f245acacdd47055a9550bac6
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-02-27 09:51:26 -0300

    Executable file

---
+++ src/management/sys/ManagementSys.java
@@ -0,0 +1,21 @@
+package management.sys;
+
+import views.LoginFrame;
+
+/**
+ *
+ * @author lurdr
+ */
+public class ManagementSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginFrame frm = new LoginFrame();
+        frm.setVisible(true);
+        
+        
+    }
+    
+}
