f7c85cb82d0beabf8e25734c0b6dffdf7a7d9002
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-05-07 14:36:14 -0300

    Moved showPrintScreen to Print class + created dataFields button control for SaleController

--- src/views/Table.java
+++ src/views/Table.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.JTableHeader;
 
 public class Table extends DefaultTableCellRenderer {
 
